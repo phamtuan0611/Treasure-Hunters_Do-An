94aebbd2948e1f18833bc7681106862abfa4d4fd
phamtuan0611 <phamtuan06112003@gmail.com>
2025-04-06 10:54:37 +0700

    Add Health Pickup

---
+++ Assets/_Game/Scripts/Pickup/HealthPickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [SerializeField] private int healthPickup;
+    [SerializeField] private GameObject pickEffect;
+    [SerializeField] private bool fullHealth;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (PlayerHealthController.instance.currentHealth != PlayerHealthController.instance.maxHealth)
+            {
+                if (fullHealth == true)
+                {
+                    PlayerHealthController.instance.AddHealth(PlayerHealthController.instance.maxHealth);
+                }
+                else
+                {
+                    PlayerHealthController.instance.AddHealth(healthPickup);
+                }
+
+                Destroy(gameObject);
+                Instantiate(pickEffect, transform.position, transform.rotation);
+                //AudioManager.instance.allSFXPlay(10);
+            }
+        }
+    }
+}
