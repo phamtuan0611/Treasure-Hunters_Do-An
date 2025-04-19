2221e482a776e5d1c833f80cb400f60d33ab183f
phamtuan0611 <phamtuan06112003@gmail.com>
2025-04-19 21:58:24 +0700

    Add Trampoline Trap

---
+++ Assets/_Game/Scripts/Traps/Trampoline/TrampolineController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineController : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private float speedBounce;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("isPlayer");
+            
+            Rigidbody2D theRB = other.GetComponent<Rigidbody2D>();
+            theRB.velocity = new Vector2(theRB.velocity.x, speedBounce);
+        }
+    }
+}
