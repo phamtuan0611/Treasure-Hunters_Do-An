253992445449378784982cda4440890b2f0f6e92
phamtuan0611 <phamtuan06112003@gmail.com>
2025-04-19 22:36:05 +0700

    Add SpikeBall Trap

---
+++ Assets/_Game/Scripts/Traps/SpikeBall/SpikeBallController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBallController : MonoBehaviour
+{
+    [SerializeField] private float initialForce = 5f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(initialForce, 0), ForceMode2D.Impulse);
+    }
+}
