c335c39848ed652afe376eb0f5bf0f344ad2bbd5
phamtuan0611 <phamtuan06112003@gmail.com>
2025-04-23 23:23:59 +0700

    Update ChestKey and Key

--- Assets/_Game/Scripts/Chest/ChestKey/ChestKeyController.cs
+++ Assets/_Game/Scripts/Chest/ChestKey/ChestKeyController.cs
@@ -1,19 +1,55 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChestKeyController : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private GameObject key, pickUp;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+            Destroy(key.gameObject);
+
             if (inventory != null && inventory.keyCount >= 1)
             {
                 anim.SetTrigger("isHaveKey");
+                boxCollider.enabled = false;
+
+
+                StartCoroutine(MoveUp());
             }
         }
+    }
+
+    private IEnumerator MoveUp()
+    {
+        yield return new WaitForSeconds(0.9f);
+
+        GameObject treasure = Instantiate(pickUp, transform.position, Quaternion.identity);
+
+        float duration = 0.5f;
+        float height = 1f;
+        Vector3 startPos = treasure.transform.position;
+        Vector3 endPos = startPos + new Vector3(0, height, 0);
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            treasure.transform.position = Vector3.Lerp(startPos, endPos, elapsed / duration);
+            elapsed += Time.deltaTime; 
+            yield return null;
+        }
+
+        treasure.transform.position = endPos;
     }
 }
--- Assets/_Game/Scripts/Chest/ChestKey/KeyController.cs
+++ Assets/_Game/Scripts/Chest/ChestKey/KeyController.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class KeyController : MonoBehaviour
 {
     [SerializeField] private GameObject effectKey;
+    private Transform targetFollow;
+    private bool isFollowing = false;
+    private Vector3 offset = new Vector3(-1.2f, 0.6f, 0f);
+    private float speed = 3f;
+
+    private void Update()
+    {
+        if (isFollowing && targetFollow != null)
+        {
+            transform.SetParent(targetFollow.transform);
+            Vector3 targetPos = targetFollow.position + offset;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, targetPos) <= 0.001)
+            {
+                isFollowing = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,9 +36,21 @@ public class KeyController : MonoBehaviour
                 inventory.CollectKey();
             }
 
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            targetFollow = other.transform;
+            isFollowing = true;
+            float direction = Mathf.Sign(other.transform.localScale.x);
+            offset.x = Mathf.Abs(offset.x) * -direction;
+            //gameObject.transform.SetParent(other.transform);
+
             GameObject effect = Instantiate(effectKey, transform.position, Quaternion.identity);
             Destroy(effect, 0.5f);
         }
+    }
+
+    private void OnDestroy()
+    {
+        GameObject effect = Instantiate(effectKey, transform.position, Quaternion.identity);
+        Destroy(effect, 0.5f);
     }
 }
