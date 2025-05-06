4209bbed94660823136c0ea97ec044d721f2e91f
phamtuan0611 <phamtuan06112003@gmail.com>
2025-05-06 10:06:52 +0700

    Update FlyingPlatform and Fan

--- Assets/_Game/Scripts/Traps/PlatformMoving.cs
+++ Assets/_Game/Scripts/Traps/PlatformMoving.cs
@@ -10,6 +10,8 @@ public class PlatformMoving : MonoBehaviour
     [SerializeField] private float timeAtPoint;
     protected float waitCounter;
 
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     public virtual void Start()
     {
@@ -19,24 +21,43 @@ public class PlatformMoving : MonoBehaviour
         }
 
         waitCounter = timeAtPoint;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    public virtual void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+    //public virtual void Update()
+    //{
+    //    transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, patrolPoints[currentPoint].position) < .001f)
+    //    if (Vector3.Distance(transform.position, patrolPoints[currentPoint].position) < .001f)
+    //    {
+    //        waitCounter -= Time.deltaTime;
+
+    //        if (waitCounter <= 0)
+    //        {
+    //            currentPoint++;
+    //            if (currentPoint >= patrolPoints.Length)
+    //            {
+    //                currentPoint = 0;
+    //            }
+    //            waitCounter = timeAtPoint;
+    //        }
+    //    }
+    //}
+
+    public virtual void FixedUpdate()
+    {
+        Vector2 targetPosition = patrolPoints[currentPoint].position;
+        Vector2 newPos = Vector2.MoveTowards(rb.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(rb.position, targetPosition) < .001f)
         {
-            waitCounter -= Time.deltaTime;
+            waitCounter -= Time.fixedDeltaTime;
 
             if (waitCounter <= 0)
             {
-                currentPoint++;
-                if (currentPoint >= patrolPoints.Length)
-                {
-                    currentPoint = 0;
-                }
+                currentPoint = (currentPoint + 1) % patrolPoints.Length;
                 waitCounter = timeAtPoint;
             }
         }
