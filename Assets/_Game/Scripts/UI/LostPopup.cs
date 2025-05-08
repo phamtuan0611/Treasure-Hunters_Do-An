d105020419f354f363cda32bcf5733433ab1c091
phamtuan0611 <phamtuan06112003@gmail.com>
2025-05-09 00:49:04 +0700

    Add Lost Popup

---
+++ Assets/_Game/Scripts/UI/LostPopup.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostPopup : MonoBehaviour
+{
+    [SerializeField] private GameObject boardLost, imageFade;
+    private bool isLost;
+
+    private void Start()
+    {
+        isLost = false;
+        imageFade.SetActive(false);
+        boardLost.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (LifeController.instance != null && LifeController.instance.currentLive == 0 && isLost == false)
+        {
+            isLost = true;
+            StartCoroutine(DelayLost());
+        }
+    }
+
+    private void PlayOpenTween()
+    {
+        boardLost.SetActive(true);
+        imageFade.SetActive(true);
+
+        boardLost.transform.DOKill();
+        boardLost.transform.localScale = Vector3.zero;
+        boardLost.transform
+            .DOScale(1f, 0.5f)
+            .SetEase(Ease.OutBack)
+            .SetUpdate(true);
+    }
+
+    IEnumerator DelayLost()
+    {
+        yield return new WaitForSeconds(1.5f);
+        PlayOpenTween();
+    }
+}
