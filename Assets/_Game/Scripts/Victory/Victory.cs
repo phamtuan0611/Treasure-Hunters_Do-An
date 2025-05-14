da49ea94ec3b140b5f84d919582baf298f71f0af
phamtuan0611 <phamtuan06112003@gmail.com>
2025-05-14 22:33:04 +0700

    Add Book Scene

--- Assets/_Game/Scripts/Victory/Victory.cs
+++ Assets/_Game/Scripts/Victory/Victory.cs
@@ -8,11 +8,12 @@ public class Victory : MonoBehaviour
 {
     [SerializeField] private GameObject board, imageFirst;
     // Start is called before the first frame update
-    public GameObject endTransitionScene;
+    public GameObject endTransitionScene, iconLoading;
     void Start()
     {
         imageFirst.SetActive(true);
         endTransitionScene.SetActive(false);
+        iconLoading.SetActive(false);
 
         RectTransform rt = board.GetComponent<RectTransform>();
         rt.DOAnchorPosY(0f, 10f).SetEase(Ease.OutCubic)
@@ -35,7 +36,7 @@ public class Victory : MonoBehaviour
     IEnumerator DelayEndTransition(string nameScene)
     {
         endTransitionScene.SetActive(true);
-
+        iconLoading.SetActive(true);
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadSceneAsync(nameScene);
         //endTransitionScene.SetActive(false);
